32f6f84221ed832656322035fd6756835bad00fe
lsooun <lsooun@gmail.com>
2019-05-30 23:16:31 +0800

    管理员可删除用户

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,7 +22,7 @@ $factory->define(User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '"$2y$10$HIIDHFSw3O7Qm1LmK7TzD.ON0fDuru13J4YbLEAENWTZVZu2R9Fly', // 123456
+        'password' => '$2y$10$HIIDHFSw3O7Qm1LmK7TzD.ON0fDuru13J4YbLEAENWTZVZu2R9Fly', // 123456
         'remember_token' => Str::random(10),
         'created_at' => $date_time,
         'updated_at' => $date_time,
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,6 +18,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'lsooun';
         $user->email = 'lsooun@gmail.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
