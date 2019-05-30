721c335f6f9dab8904a4efa480db758a621c9a74
lsooun <lsooun@gmail.com>
2019-05-30 22:58:30 +0800

    查看用户列表

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,11 +17,14 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(User::class, function (Faker $faker) {
+    $date_time = $faker->date . ' ' . $faker->time;
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => '"$2y$10$HIIDHFSw3O7Qm1LmK7TzD.ON0fDuru13J4YbLEAENWTZVZu2R9Fly', // 123456
         'remember_token' => Str::random(10),
+        'created_at' => $date_time,
+        'updated_at' => $date_time,
     ];
 });
