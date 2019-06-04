0ae1bd652c9493f9422595538db6c6a6d73db3a5
lsooun <lsooun@gmail.com>
2019-06-04 22:22:44 +0800

    用户激活

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,6 +22,7 @@ $factory->define(User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
+        'activated' => true,
         'password' => '$2y$10$HIIDHFSw3O7Qm1LmK7TzD.ON0fDuru13J4YbLEAENWTZVZu2R9Fly', // 123456
         'remember_token' => Str::random(10),
         'created_at' => $date_time,
